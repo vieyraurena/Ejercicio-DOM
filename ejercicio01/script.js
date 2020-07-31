e0b176b9b23064e05ce44f4a28663eeb88398ee3
Vieyra <vieyraurenabravo@gmail.com>
2020-07-30 20:39:35 -0600

    Agrego correcciones

--- ejercicio01/script.js
+++ ejercicio01/script.js
@@ -2,15 +2,17 @@ function getNumRamdom(max, min){
   return Math.floor(Math.random() * (max - min) + min);
 }
 
-let element = document.querySelectorAll('.container p')
-console.log(element);
+let element = document.querySelectorAll('.container p');
+
 
 function rgb(){
   for (let index = 0; index < element.length; index++){
     let red = getNumRamdom(256, 0);
     let green = getNumRamdom(256, 0);
     let blue = getNumRamdom(256, 0);
-    element[index].style.backgroundColor = `rgb(${red}, ${green}, ${blue})`;
+    element[index].style.color = `rgb(${red}, ${green}, ${blue})`;
+    const colors =  element[index].style.color;
+    console.log(colors);
   } 
 }
 
